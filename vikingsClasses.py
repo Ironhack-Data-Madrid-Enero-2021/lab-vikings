0db549484cf4d67608e9c9d8310d2351b030a33b
Antonio Garcia Giron <antoniogarciagiron@gmail.com>
2021-01-15 23:31:18 +0100

    Completo con dos fallos

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,4 +1,4 @@
-
+import random
 # Soldier
 
 class Soldier:
@@ -48,7 +48,27 @@ class War:
     def addSaxon(self, saxon):
         self.saxonArmy.append(saxon)
     def vikingAttack(self):
-        
-
-
-    
+        random.shuffle(self.vikingArmy)
+        random.shuffle(self.saxonArmy)
+        viking_fighter=self.vikingArmy[0]
+        saxon_fighter=self.saxonArmy[0]
+        attack=viking_fighter.attack()
+        if saxon_fighter.health < attack:
+            self.saxonArmy.pop(0)
+        return saxon_fighter.receiveDamage(attack)
+    def saxonAttack(self):
+        random.shuffle(self.vikingArmy)
+        random.shuffle(self.saxonArmy)
+        viking_fighter=self.vikingArmy[0]
+        saxon_fighter=self.saxonArmy[0]
+        attack=saxon_fighter.attack()
+        if viking_fighter.health < attack:
+            self.vikingArmy.pop(0)
+        return viking_fighter.receiveDamage(attack)
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif (len(self.saxonArmy) != 0) and (len(self.vikingArmy) != 0):
+            return "Vikings and Saxons are still in the thick of battle."
