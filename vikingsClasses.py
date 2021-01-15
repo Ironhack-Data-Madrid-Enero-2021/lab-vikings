015c81da81bd945b4496c78a76f5ada656639ad1
Antonio Garcia Giron <antoniogarciagiron@gmail.com>
2021-01-15 17:14:13 +0100

    Clases de soldados acabadas (Pobres sajones)

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,7 +1,6 @@
 
 # Soldier
 
-
 class Soldier:
     def __init__(self, health, strength):
         self.health=health
@@ -11,21 +10,35 @@ class Soldier:
     def receiveDamage(self, damage):
         self.health-=damage
             
-
 # Viking
 
-
-class Viking:
-    pass
+class Viking (Soldier):
+    def __init__(self, name, health, strength):
+        self.name=name
+        super().__init__(health,strength)
+    def receiveDamage(self,damage):
+        self.health-=damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
+class Saxon (Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health,strength)
+    def receiveDamage(self,damage):
+        self.health-=damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return "A Saxon has died in combat"
 
-class Saxon:
-    pass
-
+    
 # War
-
 
 class War:
     pass
