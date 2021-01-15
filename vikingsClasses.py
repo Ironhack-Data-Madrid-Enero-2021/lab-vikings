4e4e03e4181689f9d064b38812cdadfdf0243430
Jean Charles <jeancharlesyamada@MacBook-Pro-de-jean.local>
2021-01-16 00:18:35 +0100

    vikings

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -17,8 +17,23 @@ class Soldier:
 # Viking
 
 
-class Viking:
+class Viking(Soldier):
+    def __init__ (self,name,health,strength): 
+        self.name = name
+        super().__init__(health, strength)
+        
+    def attack(self):
+        return self.strength
     
+    def receiveDamage(self,damage):
+        self.health -= damage 
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"     
+    
+    def battleCry(self):
+        return f"Odin Owns You All!"
 
     pass
 
