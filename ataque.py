78adeb4cb5e6871e26faca6a0472bc99beed9192
Antonio Garcia Giron <antoniogarciagiron@gmail.com>
2021-01-16 21:34:59 +0100

    Añadido recuento de bajas

--- ataque.py
+++ ataque.py
@@ -3,6 +3,7 @@ from vikingsClasses import Viking
 from vikingsClasses import Saxon
 from vikingsClasses import Soldier
 
+
 import random
 
 names_list=['Olaf', 'Vicky', 'Ragnar', 'Harald', 'Sigurd']
@@ -17,11 +18,11 @@ for i in range(saxon_army_size):
     vikingraid.addSaxon(Saxon(random.randint(1,5), random.randint(1,5)))
     print(f"Saxon{i+1} ", vikingraid.saxonArmy[-1].health, vikingraid.saxonArmy[-1].strength)
 
-print("-"*60)
+print("-"*70)
 print("A castle in the coast of Essex, during a cold winter night...")
 print(vikingraid.vikingArmy[0].battleCry())
 fight=True
-print("-"*60)
+print("-"*70)
 turn=1
 while fight:
     print(f"Turn {turn}")
@@ -33,5 +34,23 @@ while fight:
     print(vikingraid.showStatus())
     if (len(vikingraid.saxonArmy) == 0) or (len(vikingraid.vikingArmy) == 0):
         fight=False
-    print("-"*60)
+    print("-"*70)
     
+print("-"*70)    
+print(saxon_army_size-len(vikingraid.saxonArmy), " saxons were killed in battle.")
+
+alive=[]
+dead=[]
+for viking in vikingraid.vikingArmy:
+    alive.append(viking.name)
+for viking in names_list:
+    if viking not in alive:
+        dead.append(viking)
+if (len(alive) != len(names_list)):
+    print("Vikings killed in battle are drinking with Odin in Valhalla.")  
+    print("We'll remember their names:")
+    print(", ".join(dead))
+    print('Odin Owns You All!')
+if (len(alive) == len(names_list)):
+    print("There are no casualties in the viking army.")
+print("-"*70)
