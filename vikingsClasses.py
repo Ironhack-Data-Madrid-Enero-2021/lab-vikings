30e996ebf8be6bb35486b63551533d80472fadde
Sergio <sergiocalerosanchez@gmail.com>
2021-01-16 19:52:42 +0100

    lab-vikings

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,4 +1,4 @@
-
+import random
 # Soldier
 
 
@@ -60,12 +60,54 @@ class Saxon(Soldier):
 
 
 class War:
+
     def __init__(self):
         self.vikingArmy = []
         self.saxonArmy = []
 
     def addViking(self, Viking):
-        self.vikingArmy += Viking
+        self.vikingArmy.append(Viking)#Añado un vikingo
 
-    def addSaxon
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)#Añado un sajon
+
+    def vikingAttack(self):
+        saxon_random = random.choice(self.saxonArmy)#Llamo a un sajon aleatorio
+        viking_random= random.choice(self.vikingArmy)#Llamo a un vikingo aleatorio
+        
+        #Creo una variable que almacena el daño que hace un vikingo
+        viking_attack = saxon_random.receiveDamage(viking_random.attack())
+        if saxon_random.health <= 0:
+            self.saxonArmy.remove(saxon_random)#Si el sajon no tiene vida se elimina
+
+        return viking_attack
+
+    def saxonAttack(self):
+        saxon_random = random.choice(self.saxonArmy)
+        viking_random = random.choice(self.vikingArmy)
+
+        saxon_attack = viking_random.receiveDamage(saxon_random.attack())
+
+        if viking_random.health <= 0:
+            self.vikingArmy.remove(viking_random)
+
+        return saxon_attack
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+
+        if len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+
+        if len(self.vikingArmy) and len(self.saxonArmy) == 1:
+            return "Vikings and Saxons are still in the thick of battle."
+
+
+
+
+
+
+
+    
 
