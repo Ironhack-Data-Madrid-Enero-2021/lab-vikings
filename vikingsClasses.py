1c850a03ce938d7a9881f79b26feeed41cdabff1
AnadeLuna <anadelunagm@gmail>
2021-01-17 01:23:13 +0100

    update5 sigo sin solucionar el error

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -6,8 +6,10 @@ class Soldier:
     def __init__(self,health,strength):
         self.health = health
         self.strength = strength
+    
     def attack(self):
         return self.strength
+    
     def receiveDamage(self,damage):
         self.health -= damage 
 
@@ -18,14 +20,19 @@ class Viking(Soldier):
     def __init__(self,name,health,strength):
         super().__init__(health,strength)
         self.name = name
+    
     def attack(self):
          return super().attack()
+    
     def receiveDamage(self,damage):
         self.health -= damage
+        
         if self.health > 0:
             return f"{self.name} has received {damage} points of damage"
+        
         else:
             return f"{self.name} has died in act of combat"
+    
     def battleCry(self):
         return "Odin Owns You All!"
 
@@ -33,14 +40,19 @@ class Viking(Soldier):
 
 
 class Saxon (Soldier):
+    
     def __init__(self,health,strength):
         super().__init__(health,strength)
+    
     def attack(self):
         return super().attack()
+    
     def receiveDamage(self,damage):
         self.health -= damage
+        
         if self.health > 0:
             return f"A Saxon has received {damage} points of damage"
+        
         else: 
             return "A Saxon has died in combat"
 
@@ -59,7 +71,6 @@ class War:
     def addSaxon(self,Saxon):
         self.saxonArmy.append(Saxon)
     
-    
     def vikingAttack(self):
         choiceSaxon = random.choice(self.saxonArmy)
         choiceViking = random.choice(self.vikingArmy)
@@ -72,7 +83,6 @@ class War:
         
         return attackv
     
-
     def saxonAttack(self):
         choiceSaxon = random.choice(self.saxonArmy)
         choiceViking = random.choice(self.vikingArmy)
